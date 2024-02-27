9eb41ffa88ba65c29b1c10e7d2d12254441e6904
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-02-27 16:24:37 -0500

    upd
    done

---
+++ src/src/pages/MyPhonebookPage/MyPhonebookPage.jsx
@@ -0,0 +1,12 @@
+import MyPhonebook from '../../components/MyPhonebook/MyPhonebook';
+
+const MyPhonebookPage = () => {
+  return (
+    <div>
+      <h1>My phonebooks page</h1>
+      <MyPhonebook />
+    </div>
+  );
+};
+
+export default MyPhonebookPage;
